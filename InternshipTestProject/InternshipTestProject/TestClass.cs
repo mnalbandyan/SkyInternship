3e4e527ce03b3cb6c7aff150006aa75dd14e83a0
Hripsime MELIKYAN <hripsime.melikyan@amadeus.com>
2022-07-19 16:25:48 +0400

    test

---
+++ InternshipTestProject/InternshipTestProject/TestClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InternshipTestProject
+{
+    public class TestClass
+    {
+        public TestClass()
+        {
+            Console.WriteLine("I am a TestClass's constructor");
+        }
+    }
+}
